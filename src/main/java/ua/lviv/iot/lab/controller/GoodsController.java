60349944202907c38b9aa34155c8f16c0ea34559
IgorBekerskyy <57897226+IgorBekerskyy@users.noreply.github.com>
2020-04-11 18:24:07 +0300

    Adding code and pom.xml file of my Lab 7

---
+++ src/main/java/ua/lviv/iot/lab/controller/GoodsController.java
@@ -0,0 +1,65 @@
+package ua.lviv.iot.lab.controller;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.lviv.iot.lab.model.Pen;
+
+@RequestMapping("/goods")
+@RestController
+public class GoodsController {
+  private Map<Integer, Pen> goods = new HashMap<Integer, Pen>();
+
+  private AtomicInteger idCounter = new AtomicInteger();
+
+  @GetMapping
+  public List<Pen> getGoods() {
+    return new LinkedList<Pen>(goods.values());
+  }
+
+  @GetMapping(path = "/{id}")
+  public Pen getLamp(final @PathVariable("id") Integer goodId) {
+    // to do : check if id exists
+    return goods.get(goodId);
+  }
+
+  @PostMapping
+  public Pen createGood(final @RequestBody Pen good) {
+    good.setId(idCounter.incrementAndGet());
+    goods.put(good.getId(), good);
+    return good;
+  }
+
+  @PutMapping(path = "{id}")
+  public ResponseEntity<Pen> updateGood(final @PathVariable("id") Integer goodId, final @RequestBody Pen good) {
+    if (goods.containsKey(goodId)) {
+      good.setId(goodId);
+      goods.put(goodId, good);
+      return ResponseEntity.status(HttpStatus.OK).build();
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+
+  @DeleteMapping(path = "{id}")
+  public ResponseEntity<Pen> deleteLamp(final @PathVariable("id") Integer goodId) {
+    if (goods.containsKey(goodId)) {
+      goods.remove(goodId);
+      return ResponseEntity.status(HttpStatus.OK).build();
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+  }
+}
---
+++ src/main/java/ua/lviv/iot/lab/model/AbstractGoodsForCreativity.java
@@ -0,0 +1,93 @@
+package ua.lviv.iot.lab.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import ua.lviv.iot.lab.model.KindOfProduct;
+
+public abstract class AbstractGoodsForCreativity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private String name;
+  private KindOfProduct kind;
+  private double priceInUAH;
+  private String countryOfProducing;
+  private int minimumAgeForUsing;
+
+  public AbstractGoodsForCreativity(String name, KindOfProduct kind, double priceInUAH, String countryOfProducing,
+      int minimumAgeForUsing) {
+    super();
+    this.name = name;
+    this.kind = kind;
+    this.priceInUAH = priceInUAH;
+    this.countryOfProducing = countryOfProducing;
+    this.minimumAgeForUsing = minimumAgeForUsing;
+  }
+
+  public String getHeaders() {
+    return "name," + "kind," + "priceInUAH," + "countryOfProducing," + "minimumAgeForUsing";
+
+  }
+
+  public String toCSV() {
+    return getName() + "," + getKind() + "," + getPriceInUAH() + "," + getCountryOfProducing() + ","
+        + getMinimumAgeForUsing();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public KindOfProduct getKind() {
+    return kind;
+  }
+
+  public void setKind(KindOfProduct kind) {
+    this.kind = kind;
+  }
+
+  public double getPriceInUAH() {
+    return priceInUAH;
+  }
+
+  public void setPriceInUAH(double priceInUAH) {
+    this.priceInUAH = priceInUAH;
+  }
+
+  public String getCountryOfProducing() {
+    return countryOfProducing;
+  }
+
+  public void setCountryOfProducing(String countryOfProducing) {
+    this.countryOfProducing = countryOfProducing;
+  }
+
+  public int getMinimumAgeForUsing() {
+    return minimumAgeForUsing;
+  }
+
+  public void setMinimumAgeForUsing(int minimumAgeForUsing) {
+    this.minimumAgeForUsing = minimumAgeForUsing;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public AbstractGoodsForCreativity() {
+    super();
+  }
+
+}
