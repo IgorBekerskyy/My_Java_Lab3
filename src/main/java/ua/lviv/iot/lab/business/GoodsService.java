e3d98c535a4aae56fddc1dd380123a8e4e7f814e
IgorBekerskyy <57897226+IgorBekerskyy@users.noreply.github.com>
2020-04-18 16:43:08 +0300

    Adding new classes according to my lab 8

--- src/main/java/ua/lviv/iot/lab/business/GoodsService.java
+++ src/main/java/ua/lviv/iot/lab/business/GoodsService.java
@@ -1,5 +1,39 @@
 package ua.lviv.iot.lab.business;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.lab.dataaccess.GoodsRepository;
+import ua.lviv.iot.lab.model.Pen;
+
+@Service
 public class GoodsService {
 
+    @Autowired
+    private GoodsRepository goodsRepository;
+
+    public Pen createGood(Pen pen) {
+        return goodsRepository.save(pen);
+    }
+
+    public void deleteGood(Integer goodId) {
+        goodsRepository.deleteById(goodId);
+    }
+
+    public Pen updateGood(Integer goodId, Pen pen) {
+        pen.setId(goodId);
+        return goodsRepository.save(pen);
+    }
+
+    public boolean goodExists(Integer goodId) {
+        return goodsRepository.existsById(goodId);
+    }
+
+    public Pen findGoodById(Integer goodId) {
+        return goodsRepository.findById(goodId).get();
+    }
+
+    public List<Pen> findAll() {
+        return goodsRepository.findAll();
+    }
 }
--- src/main/java/ua/lviv/iot/lab/controller/GoodsController.java
+++ src/main/java/ua/lviv/iot/lab/controller/GoodsController.java
@@ -1,10 +1,7 @@
 package ua.lviv.iot.lab.controller;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,50 +12,50 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.lviv.iot.lab.business.GoodsService;
 import ua.lviv.iot.lab.model.Pen;
 
-@RequestMapping("/goods")
+@RequestMapping("/lamps")
 @RestController
 public class GoodsController {
-  private Map<Integer, Pen> goods = new HashMap<Integer, Pen>();
 
-  private AtomicInteger idCounter = new AtomicInteger();
+    @Autowired
+    private GoodsService goodsService;
 
-  @GetMapping
-  public List<Pen> getGoods() {
-    return new LinkedList<Pen>(goods.values());
-  }
-
-  @GetMapping(path = "/{id}")
-  public Pen getGood(final @PathVariable("id") Integer goodId) {
-    return goods.get(goodId);
-  }
-
-  @PostMapping
-  public Pen createGood(final @RequestBody Pen good) {
-    good.setId(idCounter.incrementAndGet());
-    goods.put(good.getId(), good);
-    return good;
-  }
-
-  @PutMapping(path = "{id}")
-  public ResponseEntity<Pen> updateGood(final @PathVariable("id") Integer goodId, final @RequestBody Pen good) {
-    if (goods.containsKey(goodId)) {
-      good.setId(goodId);
-      goods.put(goodId, good);
-      return ResponseEntity.status(HttpStatus.OK).build();
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @GetMapping
+    public List<Pen> getGoods() {
+        return goodsService.findAll();
     }
-  }
 
-  @DeleteMapping(path = "{id}")
-  public ResponseEntity<Pen> deleteLamp(final @PathVariable("id") Integer goodId) {
-    if (goods.containsKey(goodId)) {
-      goods.remove(goodId);
-      return ResponseEntity.status(HttpStatus.OK).build();
-    } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @GetMapping(path = "/{id}")
+    public Pen getGood(final @PathVariable("id") Integer goodId) {
+        return goodsService.findGoodById(goodId);
     }
-  }
+
+    @PostMapping
+    public Pen createGood(final @RequestBody Pen good) {
+        System.out.println(goodsService.createGood(good));
+        return good;
+    }
+
+    @PutMapping(path = "{id}")
+    public ResponseEntity<Pen> updateGood(final @PathVariable("id") Integer goodId,
+            final @RequestBody Pen good) {
+        if (goodsService.goodExists(goodId)) {
+            goodsService.updateGood(goodId, good);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Pen> deleteGood(final @PathVariable("id") Integer goodId) {
+        if (goodsService.goodExists(goodId)) {
+            goodsService.deleteGood(goodId);
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
